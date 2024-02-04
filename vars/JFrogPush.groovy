c2e7fa4151d96243f406da85b77c887a109dc9bd
pacha5065 <prashantha.e@gmail.com>
2024-02-04 19:10:00 +0530

    Update JFrogPush.groovy

--- vars/JFrogPush.groovy
+++ vars/JFrogPush.groovy
@@ -1,4 +1,4 @@
 def call(){
     sh 'cd target'
-    sh 'curl -X PUT -u admin -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://15.207.14.231:8082/artifactory/example-repo-local/'
+    sh 'curl -X PUT -u admin -p Manage1234 -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://15.207.14.231:8082/artifactory/example-repo-local/'
 }
